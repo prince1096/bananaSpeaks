39cba2c566928072c5b914bedc8dc2495e891333
prince1096 <princeraj10121996@gmail.com>
2022-11-21 14:20:07 +0530

    inputOutputDone

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ console.log(btnTranslate);
 var txtInput = document.querySelector("#txt-Input");
 console.log(txtInput);
 
+var outputDiv = document.querySelector("#output");
+outputDiv.innerText = "Prince ";
+
 function clickEventHandler() {
   console.log("clicked");
   console.log(txtInput.value);
+  outputDiv.innerText = "prince" + txtInput.value;
 }
 
 // console.log(txtInput.value);
