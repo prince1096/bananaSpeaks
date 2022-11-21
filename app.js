634d74d2eb073ad1868cec67500b29535c40cbad
prince1096 <princeraj10121996@gmail.com>
2022-11-21 18:28:26 +0530

    serverApi

--- app.js
+++ app.js
@@ -7,12 +7,36 @@ var txtInput = document.querySelector("#txt-Input");
 console.log(txtInput);
 
 var outputDiv = document.querySelector("#output");
-outputDiv.innerText = "Prince ";
+// outputDiv.innerText = "Prince ";
+
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslation(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+}
 
 function clickEventHandler() {
-  console.log("clicked");
-  console.log(txtInput.value);
-  outputDiv.innerText = "prince" + txtInput.value;
+  //   console.log("clicked");
+  //   console.log(txtInput.value);
+  //   outputDiv.innerText = "prince" + txtInput.value;
+
+  var inputText = txtInput.value;
+
+  // calling server for processing
+  fetch(getTranslation(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var finalText = json.contents.translated;
+      outputDiv.innerText = finalText;
+    })
+    // console.log(json.contents.translated))
+    .catch(errorHandler);
 }
 
 // console.log(txtInput.value);
